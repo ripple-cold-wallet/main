bd646db0eb084f13a4a56d6273935b6290ed687e
Amelito N. Alcuitas Jr <amelitoalcuitasjr@gmail.com>
2017-06-24 18:25:52 +0800

    updated everything

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,26 @@
 var qrcode1 = new QRCode("qrcode1", {
-    width: 140,
-    height: 140,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
+    width: 145,
+    height: 145,
+    colorDark : "#000",
+    colorLight : "#fff",
     correctLevel : QRCode.CorrectLevel.H
 });
 
 var qrcode2 = new QRCode("qrcode2", {
     width: 130,
     height: 130,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
+    colorDark : "#000",
+    colorLight : "#fff",
     correctLevel : QRCode.CorrectLevel.H
 });
 
 var qrcode3 = new QRCode("qrcode3", {
-    width: 130,
-    height: 130,
-    colorDark : "#ffffff",
-    colorLight : "#000000",
+    width: 128,
+    height: 128,
+    colorDark : "#000",
+    colorLight : "#fff",
     correctLevel : QRCode.CorrectLevel.H
 });
-
 
 window.onload = function() {
   generateKey();
@@ -35,12 +34,13 @@ function generateKey() {
   complete: function(data) {
     qr1 = data.responseJSON.address;
     qr2 = data.responseJSON.secret;
-    console.log("public: " + data.responseJSON.secret);
+    // console.log("public: " + data.responseJSON.secret);
     qrcode1.makeCode(qr1);
     qrcode2.makeCode(qr2);
     qrcode3.makeCode(qr1);
 
     $('.receive').html(qr1);
+    $('.receive2').html(qr1);
     $('.send').html(qr2);
   }
 });
@@ -49,20 +49,19 @@ function generateKey() {
 }
 
 $('#generate').click(function(){
-  qr1 = $('p.receive').text();
-  qr2 = generateKey();
-
-  qrcode1.makeCode(qr1);
-  qrcode2.makeCode(qr2);
-
-  $('.receive').html(qr1);
-  $('.send').html(qr2);
+  generateKey();
 });
 
 $('#clear').click(function(){
-  qrcode1.clear();
-  qrcode2.clear();
+  // qrcode1.clear(); // NOT WORKING DUNNO WHY
+  // qrcode2.clear(); // NOT WORKING DUNNO WHY
+  // qrcode3.clear(); // NOT WORKING DUNNO WHY
+
+  $('#qrcode1 img').attr('src','');
+  $('#qrcode2 img').attr('src','');
+  $('#qrcode3 img').attr('src','');
 
   $('.receive').html('');
+  $('.receive2').html('');
   $('.send').html('');
 });
